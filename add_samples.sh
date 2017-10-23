3266f9e91101ee4454d99f9b6e4ae12fbac0b363
Andreas Hinzmann <hinzmann@cern.ch>
2017-10-23 15:57:17 +0200

    update readme

---
+++ add_samples.sh
@@ -0,0 +1,4 @@
+hadd -f -k dijetChiRun2017_RunB_PromptReco-v1.root workdir/dijetChiRun2017_RunB_PromtReco-v1_*.root
+hadd -f -k dijetChiRun2017_RunC_PromptReco-v1.root workdir/dijetChiRun2017_RunC_PromtReco-v1_*.root
+hadd -f -k dijetChiRun2017_RunD_PromptReco-v1.root workdir/dijetChiRun2017_RunD_PromtReco-v1_*.root
+hadd -f -k uhh2.AnalysisModuleRunner.DATA.Run2017_RunBCD_PromtReco-v1.root workdir/uhh2.AnalysisModuleRunner.DATA.Run2017_Run*.root
