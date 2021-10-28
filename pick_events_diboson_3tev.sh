feb878fadba93eb1a9e1f05aa16867ade24e0027
hinzmann <hinzmann@cern.ch>
2021-10-28 17:51:35 +0200

    diboson event displays

---
+++ pick_events_diboson_3tev.sh
@@ -0,0 +1,39 @@
+rm ~/.dasmaps/das_maps_dbs_prod.js
+
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2018A-17Sep2018-v1/AOD" --output dibosonpick2018A events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2018B-17Sep2018-v1/AOD" --output dibosonpick2018B events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2018C-17Sep2018-v1/AOD" --output dibosonpick2018C events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2018D-PromptReco-v1/AOD" --output dibosonpick2018D1 events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2018D-PromptReco-v2/AOD" --output dibosonpick2018D2 events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2017B-17Nov2017-v1/AOD" --output dibosonpick2017B events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2017C-17Nov2017-v1/AOD" --output dibosonpick2017C events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2017D-17Nov2017-v1/AOD" --output dibosonpick2017D events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2017E-17Nov2017-v1/AOD" --output dibosonpick2017E events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2017F-17Nov2017-v1/AOD" --output dibosonpick2017F events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2016B-07Aug17_ver1-v1/AOD" --output dibosonpick2016B1 events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2016B-07Aug17_ver2-v1/AOD" --output dibosonpick2016B2 events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2016C-07Aug17-v1/AOD" --output dibosonpick2016C events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2016D-07Aug17-v1/AOD" --output dibosonpick2016D events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2016E-07Aug17-v1/AOD" --output dibosonpick2016E events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2016F-07Aug17-v1/AOD" --output dibosonpick2016F events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2016G-07Aug17-v1/AOD" --output dibosonpick2016G events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2016H-07Aug17-v1/AOD" --output dibosonpick2016H events_diboson_3tev.txt
+
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2018A-17Sep2018-v1/MINIAOD" --output dibosonpick2018Amini events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2018B-17Sep2018-v1/MINIAOD" --output dibosonpick2018Bmini events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2018C-17Sep2018-v1/MINIAOD" --output dibosonpick2018Cmini events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2018D-PromptReco-v1/MINIAOD" --output dibosonpick2018D1mini events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2018D-PromptReco-v2/MINIAOD" --output dibosonpick2018D2mini events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2017B-31Mar2018-v1/MINIAOD" --output dibosonpick2017Bmini events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2017C-31Mar2018-v1/MINIAOD" --output dibosonpick2017Cmini events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2017D-31Mar2018-v1/MINIAOD" --output dibosonpick2017Dmini events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2017E-31Mar2018-v1/MINIAOD" --output dibosonpick2017Emini events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2017F-31Mar2018-v1/MINIAOD" --output dibosonpick2017Fmini events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2016B-17Jul2018_ver1-v1/MINIAOD" --output dibosonpick2016B1mini events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2016B-17Jul2018_ver2-v2/MINIAOD" --output dibosonpick2016B2mini events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2016C-17Jul2018-v1/MINIAOD" --output dibosonpick2016Cmini events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2016D-17Jul2018-v1/MINIAOD" --output dibosonpick2016Dmini events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2016E-17Jul2018-v1/MINIAOD" --output dibosonpick2016Emini events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2016F-17Jul2018-v1/MINIAOD" --output dibosonpick2016Fmini events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2016G-17Jul2018-v1/MINIAOD" --output dibosonpick2016Gmini events_diboson_3tev.txt
+./edmPickEvents.py --runInteractive --maxEventsInteractive=2000 "/JetHT/Run2016H-17Jul2018-v1/MINIAOD" --output dibosonpick2016Hmini events_diboson_3tev.txt
